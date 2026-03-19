759df10bfca9190996c7e85956394fc8dfe11102
Tim <timrealdev@outlook.com>
2026-03-19 17:09:26 +1000

    feat: add document class

---
+++ Models/Document.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineNotePad.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+    }
+}
